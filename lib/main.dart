3d9396873130e0e445f1d6b9f653ed66338340f1
Sylva <sylvaintagnabou@gmail.com>
2022-08-18 15:41:48 +0000

    testing intro-screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,61 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tic Tac Toe',
-      home: HomePage(),
+      home: IntroScreen(),
+    );
+  }
+}
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> with SingleTickerProviderStateMixin {
+
+    static var myNewFront = GoogleFonts.pressStart2p(
+      textStyle: TextStyle(color: Colors.black, letterSpacing: 3));
+  static var myNewFontWite = GoogleFonts.pressStart2p(
+      textStyle:
+          TextStyle(color: Colors.white, letterSpacing: 3));
+  @override
+  Widget build(BuildContext context) {
+    final color =Colors.white;
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 120.0),
+                  child: Container(
+                    child: Text(
+                      'TIC TAC TOE',
+                      style:myNewFontWite.copyWith(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: AvatarGlow(
+                    endRadius: 140,
+                    duration: Duration(2s),
+                    glowColor: color,
+                    repeat: ,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
