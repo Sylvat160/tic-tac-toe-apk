68eaca8cf0d7b763f5af7dc9b4b8c140f74f335a
Sylva <sylvaintagnabou@gmail.com>
2022-08-18 12:11:51 +0000

    first step passed.

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
+      debugShowCheckedModeBanner: false,
       title: 'Tic Tac Toe',
       home: HomePage(),
     );
   }
 }
-class HomePage extends StatefulWidget{
-  const HomePage({Key?key}):super(key: key);
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
-  
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  List<String> displayExOh = ['','','','','','','','',''];
 
-  String displayExOh ='';
-  
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: GridView.builder(
         itemCount: 9,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), 
-        itemBuilder: (BuildContext context, int index){
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(color:Colors.grey)
-            ),
-            child: Center(
-              child: Text(
-                displayExOh, 
-                style : const TextStyle(
-                  color: Colors.white , 
-                  fontSize: 40
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: (){
+              _tapped(index);
+            },
+            child: Container(
+              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+              child: Center(
+                child: Text(
+                  displayExOh[index],
+                  style: const TextStyle(color: Colors.white, fontSize: 40),
                 ),
-                ),
+              ),
             ),
           );
         },
-        ),
+      ),
     );
+  }
+
+
+  void _tapped(int index){
+    setState(() {
+      displayExOh[index] = 'o';
+    });
+  }
+
+  void _checkWinner(){
+
+  }
+  void _checkWinDialog(){
+    
   }
 }
